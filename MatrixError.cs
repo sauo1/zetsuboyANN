0735772680d3b20ab65c600be51fe96c90bbed60
Николай <larenso1991@hotmail.com>
2018-02-18 07:55:30 +0900

    first build

---
+++ MatrixError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace zetsuboyANN
+{
+    class MatrixError : System.Exception
+    {
+        /// <summary>
+        /// Constructor for a simple message exception.
+        /// </summary>
+        /// <param name="str">The message for the exception.</param>
+        public MatrixError(String str) : base(str)
+        {
+        }
+    }
+}
